c92f9123043637d7d2448d740735a0c988035efc
Chris Betker <c_betker@hotmail.com>
2011-12-20 17:59:31 -0600

    started working on updating paths

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,84 +1,42 @@
 class ForumsController < ApplicationController
-  # GET /forums
-  # GET /forums.xml
   def index
     @forums = Forum.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @forums }
-    end
   end
-
-  # GET /forums/1
-  # GET /forums/1.xml
+  
   def show
     @forum = Forum.find(params[:id])
     @topics = Topic.where("forum_id = ?", params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @forum }
-    end
   end
-
-  # GET /forums/new
-  # GET /forums/new.xml
+  
   def new
     @forum = Forum.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @forum }
-    end
   end
-
-  # GET /forums/1/edit
+  
   def edit
     @forum = Forum.find(params[:id])
   end
-
-  # POST /forums
-  # POST /forums.xml
+  
   def create
     @forum = Forum.new(params[:forum])
-
-    respond_to do |format|
-      if @forum.save
-        format.html { redirect_to(@forum, :notice => 'Forum was successfully created.') }
-        format.xml  { render :xml => @forum, :status => :created, :location => @forum }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @forum.errors, :status => :unprocessable_entity }
-      end
+    if @forum.save
+      redirect_to(@forum, :notice => 'Forum was successfully created.')
+    else
+      render :action => "new"
     end
   end
-
-  # PUT /forums/1
-  # PUT /forums/1.xml
+  
   def update
     @forum = Forum.find(params[:id])
-
-    respond_to do |format|
-      if @forum.update_attributes(params[:forum])
-        format.html { redirect_to(@forum, :notice => 'Forum was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @forum.errors, :status => :unprocessable_entity }
-      end
+    if @forum.update_attributes(params[:forum])
+      redirect_to(@forum, :notice => 'Forum was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
-
-  # DELETE /forums/1
-  # DELETE /forums/1.xml
+  
   def destroy
     @forum = Forum.find(params[:id])
     @forum.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(forums_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(forums_url)
   end
 end
